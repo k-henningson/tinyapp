9fc53abec7d64b5693b760a0ab7bc9a907ddc2ae
Kyra Henningson <kyrahenningson@hotmail.com>
2022-09-06 23:59:49 +0000

    Displayed username on render templates

--- express_server.js
+++ express_server.js
@@ -36,16 +36,17 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { urls: urlDatabase,  username: req.cookies["username"]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { username: req.cookies["username"]};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id]}
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]}
   res.render("urls_show", templateVars);
 });
 
